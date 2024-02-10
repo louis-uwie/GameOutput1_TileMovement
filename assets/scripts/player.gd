900aac2de3f790b4f55d221aa443005205310691
Angelico Teaño <angelico.teano@obf.ateneo.edu>
2024-02-10 17:25:36 +0800

    Add player animation and Fix camera limits

--- assets/scripts/player.gd
+++ assets/scripts/player.gd
@@ -2,20 +2,26 @@ extends Node2D
 
 var player_speed = 200
 var player_velocity = Vector2()
+@onready var animation = $AnimatedSprite2D
 
 func _ready():
+	animation.play("idle")
 	pass
 
 func _process(delta):
 	player_velocity = Vector2()
 
 	if Input.is_action_pressed("ui_right"):
+		animation.play("right")
 		player_velocity.x += 1
 	if Input.is_action_pressed("ui_left"):
+		animation.play("left")
 		player_velocity.x -= 1
 	if Input.is_action_pressed("ui_down"):
+		animation.play("down")
 		player_velocity.y += 1
 	if Input.is_action_pressed("ui_up"):
+		animation.play("up")
 		player_velocity.y -= 1
 
 	# Normalize diagonal movement
