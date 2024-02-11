cf02a0719472e6547aa3001df25c8d6f08e33e0e
uwie <157711065+louis-uwie@users.noreply.github.com>
2024-02-11 14:05:11 +0800

    Update collisions branch (#10)
    * Add character obstacles and player idle animations (#7)
    
    Co-authored-by: Angelico Teaño <angelico.teano@obf.ateneo.edu>
    
    * Grid-based movement patch (#9)
    
    * Fixed player movement
    
    Made it GRID BASED, and Stops on the middle of the tile.
    
    * Add character obstacles and player idle animations (#7) (#8)
    
    Co-authored-by: Angelico Teaño <angelico.teano@obf.ateneo.edu>
    
    ---------
    
    Co-authored-by: Angelico Teaño <angelico.teano@obf.ateneo.edu>
    
    ---------
    
    Co-authored-by: Angelico Teaño <angelico.teano@obf.ateneo.edu>

--- assets/scripts/player.gd
+++ assets/scripts/player.gd
@@ -2,31 +2,61 @@ extends Node2D
 
 var player_speed = 200
 var player_velocity = Vector2()
+var goal_position = Vector2()
+
+var is_moving = false
 @onready var animation = $AnimatedSprite2D
 
 func _ready():
 	animation.play("idle")
-	pass
-
-func _process(delta):
 	player_velocity = Vector2()
 
-	if Input.is_action_pressed("ui_right"):
-		animation.play("right")
-		player_velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-		animation.play("left")
-		player_velocity.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		animation.play("down")
-		player_velocity.y += 1
-	if Input.is_action_pressed("ui_up"):
-		animation.play("up")
-		player_velocity.y -= 1
+func _process(delta):
 
-	# Normalize diagonal movement
-	player_velocity = player_velocity.normalized()
+	if is_moving:
+		# Normalize diagonal movement
+		player_velocity = player_velocity.normalized()
+		
+		# Update player position based on input and speed
+		var new_position = get_position() + player_velocity * player_speed * delta
+		set_position(new_position)
 
-	# Update player position based on input and speed
-	var new_position = get_position() + player_velocity * player_speed * delta
-	set_position(new_position)
+		print(new_position)
+		print(goal_position)
+		
+		var distance = sqrt((new_position.x - goal_position.x)**2 + (new_position.y - goal_position.y)**2)
+		
+		if distance <= 16:
+			player_velocity = Vector2()
+			set_position(goal_position)
+			is_moving = false
+		
+		print("we're okay")
+	else:
+		if Input.is_action_pressed("ui_right"):
+			animation.play("right")
+			player_velocity.x += 1
+			goal_position = get_position() + player_velocity * 32
+			is_moving = true
+			
+		elif Input.is_action_pressed("ui_left"):
+			animation.play("left")
+			player_velocity.x -= 1
+			goal_position = get_position() + player_velocity * 32
+			is_moving = true
+			
+		elif Input.is_action_pressed("ui_down"):
+			animation.play("down")
+			player_velocity.y += 1
+			goal_position = get_position() + player_velocity * 32
+			is_moving = true
+			
+		elif Input.is_action_pressed("ui_up"):
+			animation.play("up")
+			player_velocity.y -= 1
+			goal_position = get_position() + player_velocity * 32
+			is_moving = true
+			
+		else:
+			animation.play("idle")
+		
