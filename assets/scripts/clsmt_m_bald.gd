7f1baefe4860a535d0b4fd43d3c4502772b7cd25
uwie <louis.binwag@student.ateneo.edu>
2024-02-12 06:13:14 +0800

    Final Changes
    Movement
    ForceMovement
    Animations
    Teleport Animation

--- assets/scripts/clsmt_m_bald.gd
+++ assets/scripts/clsmt_m_bald.gd
@@ -4,40 +4,48 @@ extends CharacterBody2D
 @onready var ray = $RayCast2D
 const tile_size = 32
 const animation_speed = 8
-var direction = Vector2.DOWN
+var direction = Vector2.RIGHT
 var animating = false
+var idle_face = ["idle_up", "idle_down"]
 
 func _ready():
-	animations.play("down")
+	animations.play("right")
 
 func _animate(dir):
 	ray.target_position = dir * tile_size
 	ray.force_raycast_update()
 	
 	if !ray.is_colliding() and !animating:
-		if dir == Vector2.DOWN:
-			animations.play("down")
+		if dir == Vector2.LEFT:
+			animations.play("left")
 		else:
-			animations.play("up")
+			animations.play("right")
 		
 		var tween = create_tween()
 		tween.tween_property(self, "position", position + dir * tile_size, 1.0/animation_speed).set_trans(Tween.TRANS_SINE)
 		animating = true
 		await tween.finished
 		animating = false
+		
 	elif ray.is_colliding() and !animating:
-		if dir == Vector2.DOWN:
-			animations.play("idle_down")
+		if dir == Vector2.LEFT:
+			animations.play("idle_left")
 		else:
-			animations.play("idle_up")
+			animations.play("idle_right")
 	
 func _physics_process(delta):
-	if position.y < 176:
-		direction = Vector2.DOWN
+	if position.x <= 240 and !animating:
+		animations.play(idle_face[randi_range (0, 1)])
+		animating = true
+		await get_tree().create_timer(3).timeout
+		animating = false
+		direction = Vector2.RIGHT
 		
-	elif position.y > 496:
-		direction = Vector2.UP
+	elif position.x >= 336 and !animating:
+		animations.play(idle_face[randi_range (0, 1)])
+		animating = true
+		await get_tree().create_timer(3).timeout
+		animating = false
+		direction = Vector2.LEFT
 	
 	_animate(direction)
-	
-	
--- assets/scripts/player.gd
+++ assets/scripts/player.gd
@@ -3,9 +3,13 @@ extends CharacterBody2D
 var tile_size = 32
 
 var last_direction = ""
-var is_teleporting = false
 var tp_here = Vector2(816,400)
+
 var animate = false
+var is_teleporting = false
+var is_pushing = false
+var is_slipping = false
+
 const animation_speed = 8
 
 @onready var animation = $AnimatedSprite2D
@@ -14,8 +18,31 @@ const animation_speed = 8
 func _ready():
 	animation.play("idle_down")
 	
-func _process(event):
-	if animate or is_teleporting:
+func _physics_process(event):
+	if is_pushing and !animate:
+		if position.y < 847:
+			move(Vector2.DOWN,"ui_down")
+			animation.play("down")
+		else:
+			is_pushing = false
+			animation.play("idle_down")
+
+	if is_slipping and !animate:
+		if last_direction == "down":
+			move(Vector2.DOWN, "down")
+			animation.play("idle_down")
+		elif last_direction == "up":
+			move(Vector2.UP, "up")
+			animation.play("idle_up")
+		elif last_direction == "right":
+			move(Vector2.RIGHT, "right")
+			animation.play("idle_right")
+		elif last_direction == "left":
+			move(Vector2.LEFT, "left")
+			animation.play("idle_left")
+		print(last_direction)
+			
+	if animate or is_teleporting or is_pushing:
 		return
 		
 	if Input.is_action_pressed("ui_up"):
@@ -28,6 +55,8 @@ func _process(event):
 		move(Vector2.LEFT, "left")
 	else:
 		animation.play("idle_"+last_direction)
+		
+	
 
 func move(dir, string_dir):	
 	ray.target_position = dir * tile_size
@@ -38,15 +67,13 @@ func move(dir, string_dir):
 			
 		var tween = create_tween()
 		tween.tween_property(self, "position",
-			position + dir *    tile_size, 1.0/animation_speed).set_trans(Tween.TRANS_SINE)
+			position + dir * tile_size, 1.0/animation_speed).set_trans(Tween.TRANS_SINE)
 		animate = true
 		await tween.finished
 		animate = false
 	else:
 		animation.play("idle_"+string_dir)
 	
-
-
 func _on_area_2d_body_entered(body):
 	print("entered")
 	is_teleporting = true
@@ -56,3 +83,17 @@ func _on_area_2d_body_entered(body):
 	is_teleporting = false
 	set_position(tp_here)
 	pass # Replace with function body.
+
+func _on_area_2d_2_body_entered(body):
+	is_pushing = true
+
+
+func _on_area_2d_3_body_entered(body):
+	print("entered")
+	is_slipping = true
+
+func _on_area_2d_3_body_exited(body):
+	is_slipping = false
+	print("exited")
+
+
